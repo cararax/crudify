8d3f11b05f0283136568313cd622411684e60a1f
Pedro Carara <cararax@gmail.com>
2023-07-30 07:44:03 -0300

    initial commit 🎉

---
+++ src/main/java/com/carara/crudify/CrudifyApplication.java
@@ -0,0 +1,13 @@
+package com.carara.crudify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carara/crudify/CrudifyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.carara.crudify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class CrudifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
